90d8d5ebe537b97ad9b7f95158f4f02d008b5bcf
Charvit <charvitardeshna@gmail.com>
2025-04-03 20:09:38 +0530

    Get session-id

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String helloMethod(HttpServletRequest request) {
+        return "Hello peeps, welcome to the cherry's world"+request.getSession().getId();
+    }
+
+}
