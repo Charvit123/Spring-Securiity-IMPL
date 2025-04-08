5ba87d36da7c810be28dcac4d2600eaf1f03249d
Charvit <charvitardeshna@gmail.com>
2025-04-08 17:32:41 +0530

    some modifications and remove unused imports

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,6 +1,6 @@
 package com.example.demo.controller;
 
-import com.example.demo.entity.Student;
+import com.example.demo.model.Student;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/com/example/demo/service/JWTService.java
+++ src/main/java/com/example/demo/service/JWTService.java
@@ -11,7 +11,10 @@ import org.springframework.stereotype.Service;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import java.security.NoSuchAlgorithmException;
-import java.util.*;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
